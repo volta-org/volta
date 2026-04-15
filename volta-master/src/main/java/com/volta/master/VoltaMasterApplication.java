dd6e87de60993e89a50cf7ad0383dd89401cb897
marr97 <132494156+marr97@users.noreply.github.com>
2026-04-15 15:38:58 +0300

    feature: add basic Spring Boot set up for master module (#35)
    Co-authored-by: Alexandr Baranov <sani86188@gmail.com>

---
+++ volta-master/src/main/java/com/volta/master/VoltaMasterApplication.java
@@ -0,0 +1,11 @@
+package com.volta.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoltaMasterApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(VoltaMasterApplication.class, args);
+  }
+}
