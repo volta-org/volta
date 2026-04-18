c93955de662ae185c1f345d36dfb85481727c74c
p1gushka <sani86188@gmail.com>
2026-04-19 00:31:11 +0300

    feature: implement CLI args parsing in Master (#40)

---
+++ volta-master/src/main/java/com/volta/master/StartupRunner.java
@@ -0,0 +1,37 @@
+package com.volta.master;
+
+import com.volta.master.cli.ArgsException;
+import com.volta.master.cli.ArgsParser;
+import com.volta.master.cli.MasterArgs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupRunner implements ApplicationRunner {
+
+  private static final Logger log = LoggerFactory.getLogger(StartupRunner.class);
+
+  @Override
+  public void run(ApplicationArguments args) {
+    MasterArgs masterArgs;
+    try {
+      masterArgs = ArgsParser.parse(args);
+    } catch (ArgsException e) {
+      log.error(e.getMessage());
+      System.exit(1);
+      return;
+    }
+
+    log.info(
+        "Starting test: url={}, rps={}, duration={}s, agent={}",
+        masterArgs.testConfig().url(),
+        masterArgs.testConfig().rps(),
+        masterArgs.testConfig().duration(),
+        masterArgs.agentUrl());
+
+    // agentClient.startTest(masterArgs.agentUrl(), masterArgs.testConfig());
+  }
+}
---
+++ volta-master/src/main/java/com/volta/master/cli/ArgsException.java
@@ -0,0 +1,7 @@
+package com.volta.master.cli;
+
+public class ArgsException extends RuntimeException {
+  public ArgsException(String message) {
+    super(message);
+  }
+}
