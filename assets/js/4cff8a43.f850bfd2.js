053042232e237a3cfb4bb0b6d73db25d2fdf17f6
p1gushka <p1gushka@users.noreply.github.com>
2026-05-06 15:12:35 +0000

    deploy: 30e1e542a3d72619a978819a5317935de65af81e

---
+++ assets/js/4cff8a43.f850bfd2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([["639"],{7173(e){e.exports=JSON.parse('{"metadata":{"permalink":"/volta/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
